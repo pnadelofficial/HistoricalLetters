a8c4f99f9240483b402f8c7827b4294d07aae1ef
PatrickFeeney <patrickfeeney1225@gmail.com>
2020-11-18 15:00:17 -0500

    Better Plotting

--- graph.py
+++ graph.py
@@ -5,7 +5,7 @@ import numpy as np
 def to_from(letter_array):
     to_from = letter_array[:, [1, 5]]
     to_from = remove_empty(to_from)
-    to_from_graph = nx.convert.from_edgelist(to_from, create_using=nx.MultiGraph)
+    to_from_graph = nx.convert.from_edgelist(to_from, create_using=nx.MultiDiGraph)
     return to_from_graph
 
 
--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ import networkx as nx
 
 
 def draw_planar(graph):
+    plt.figure(1, figsize=(8, 4.5))
     pos = nx.planar_layout(graph)
-    nx.draw_networkx(graph, pos=pos, with_labels=True)
+    nx.draw_networkx(graph, pos=pos, with_labels=True, edge_color="blue")
     plt.show()
