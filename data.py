b40f42eb1d646425d164a931869388d0a838b4cc
PatrickFeeney <patrickfeeney1225@gmail.com>
2020-11-18 11:40:56 -0500

    New Data

--- data.py
+++ data.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
+def load_heirarchy(fname):
+    names = np.loadtxt(fname, delimiter='\t', skiprows=1, dtype=str, usecols=0)
+    names = remove_comments(names[np.newaxis])[0]
+    ranks = np.loadtxt(fname, delimiter='\t', skiprows=1, dtype=int, usecols=1)
+    return dict(zip(names, ranks))
+
+
 def load_letters(fname):
     letters = np.loadtxt(fname, delimiter='\t', skiprows=1, dtype=str)
-    # remove comments
-    for i in range(letters.shape[0]):
-        for j in range(letters.shape[1]):
-            index = max(letters[i, j].find('('), letters[i, j].find('['))
-            if index > -1:
-                letters[i, j] = letters[i, j][:index]
-    # remove whitespace
-    letters = np.char.strip(letters)
+    letters = remove_comments(letters)
     # fix incorrect names
     name_dict = {"Lord J. Butler": "Lord John Butler",
                  "Sir John Warr": "John Warre",
@@ -18,3 +18,15 @@ def load_letters(fname):
     for bad_name, new_name in name_dict.items():
         letters[letters == bad_name] = new_name
     return letters
+
+
+def remove_comments(str_array):
+    # remove comments
+    for i in range(str_array.shape[0]):
+        for j in range(str_array.shape[1]):
+            index = max(str_array[i, j].find('('), str_array[i, j].find('['))
+            if index > -1:
+                str_array[i, j] = str_array[i, j][:index]
+    # remove whitespace
+    str_array = np.char.strip(str_array)
+    return str_array
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import graph
 import plot
 
 
-letter_array = data.load_letters("data/BeinekieRecordLetters.tsv")
-to_from_graph = graph.to_from(letter_array)
-plot.draw_planar(to_from_graph)
+hierarchy = data.load_heirarchy("data/Hierarchy.tsv")
+letter_sets = ["data/BeinekieRecordLetters.tsv",
+               "data/BeinekieRecordLetters2.tsv"]
+for letter_set in letter_sets:
+    letter_array = data.load_letters(letter_set)
+    to_from_graph = graph.to_from(letter_array)
+    plot.draw_planar(to_from_graph)
