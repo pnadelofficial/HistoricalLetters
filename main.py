399879c041bec192ebbf578b4d90baed6e4e80ab
PatrickFeeney <patrickfeeney1225@gmail.com>
2020-11-23 13:37:17 -0500

    Reorganizing main.py

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import plot
 hierarchy = data.load_hierarchy("data/Hierarchy.tsv")
 letter_sets = ["data/BeinekieRecordLetters.tsv",
                "data/BeinekieRecordLetters2.tsv"]
+to_from_graphs = []
 for letter_set in letter_sets:
     letter_array = data.load_letters(letter_set)
     to_from_graph = graph.to_from(letter_array)
+    to_from_graphs += [to_from_graph]
     if letter_set == "data/BeinekieRecordLetters2.tsv":
-        before_graph, after_graph = graph.to_from_dates(letter_array)
-    print(letter_set)
+        to_from_graphs += [*graph.to_from_dates(letter_array)]
+
+for to_from_graph in to_from_graphs:
     for node in to_from_graph.nodes:
         if node not in hierarchy:
             print(node)
