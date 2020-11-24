3d1da5225e6326eafecf9d87706d29f9b938f53e
PatrickFeeney <patrickfeeney1225@gmail.com>
2020-11-24 12:40:16 -0500

    People Statistics

--- main.py
+++ main.py
@@ -26,13 +26,14 @@ graph_names = ["BeinekieRecordLetters",
 for i, to_from_graph in enumerate(to_from_graphs):
     # add rank node attributes and make rank dictionary
     rank_dict = {}
-    for j, node in enumerate(to_from_graph.nodes):
-        if node not in hierarchy:
-            print(node)
+    names = list(to_from_graph.nodes)
+    for j, name in enumerate(names):
+        if name not in hierarchy:
+            print(name)
             rank = 10
         else:
-            rank = hierarchy[node]
-        to_from_graph.nodes[node]["rank"] = rank
+            rank = hierarchy[name]
+        to_from_graph.nodes[name]["rank"] = rank
         if rank not in rank_dict:
             rank_dict[rank] = []
         rank_dict[rank] += [j]
@@ -58,11 +59,25 @@ for i, to_from_graph in enumerate(to_from_graphs):
             rank_stats[j, k] = num_from_to/len(from_idx)
     # total number of letters
     rank_stats[-1, -1] = np.sum(rank_stats[-1])
+    # add labels to rank statistics
     pd_rank_stats = pd.DataFrame(rank_stats,
                                  index=["Average From " + rank for rank in ranks.astype(str)] +
                                        ["Total Letters Received"],
                                  columns=["To " + rank for rank in ranks.astype(str)] +
                                          ["Total Letters Sent"])
     pd_rank_stats.to_csv("output/Rank%s.csv" % (graph_names[i]))
+    # get statistics for each person
+    person_stats = np.zeros((len(names), 2 * num_ranks), dtype=int)
+    for j, rank in enumerate(ranks):
+        # letters to people from rank
+        person_stats[:, j] = np.sum(np_adj[rank_dict[rank]], axis=0)
+        # letters from rank to people
+        person_stats[:, j + num_ranks] = np.sum(np_adj[:, rank_dict[rank]], axis=1)
+    # add labels to person statistics
+    pd_person_stats = pd.DataFrame(person_stats,
+                                   index=names,
+                                   columns=["To " + rank for rank in ranks.astype(str)] +
+                                           ["From " + rank for rank in ranks.astype(str)])
+    pd_person_stats.to_csv("output/People%s.csv" % (graph_names[i]))
     # plot the graph
     plot.draw_planar(to_from_graph)
