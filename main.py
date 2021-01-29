5d35f6621dd46111b8f8ef3e0bcb91d63eb46d19
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-01-29 15:03:58 -0500

    String Type Fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import data
 
-fname = r"canonical-latinLit/data/phi0474/phi056/phi0474.phi056.perseus-lat1.xml"
+fname = "canonical-latinLit/data/phi0474/phi056/phi0474.phi056.perseus-lat1.xml"
 parser = data.EpiDocXMLParser(fname)
 dateline_text = parser.dateline_text()[:, np.newaxis]
 np.savetxt("output/phi056dateline.csv", dateline_text, fmt="%s", delimiter=',')
