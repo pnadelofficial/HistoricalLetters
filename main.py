0e0151ae2250c511f983cb4bfdea96b31e2fed13
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-01-29 15:17:48 -0500

    Added CSV Quotes

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import data
 fname = "canonical-latinLit/data/phi0474/phi056/phi0474.phi056.perseus-lat1.xml"
 parser = data.EpiDocXMLParser(fname)
 dateline_text = parser.dateline_text()[:, np.newaxis]
-np.savetxt("output/phi056dateline.csv", dateline_text, fmt="%s", delimiter=',')
+np.savetxt("output/phi056dateline.csv", dateline_text, fmt='"%s"', delimiter=',')
