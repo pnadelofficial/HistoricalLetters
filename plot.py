0cc1e97a8739c53db92e6910b3c61541a1be1abd
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-03-04 13:17:01 -0500

    Cleaner Plotting

--- plot.py
+++ plot.py
@@ -5,20 +5,19 @@ import numpy as np
 
 
 def plot_geodetic(geodetic_coords):
-    # set up map imagery
+    # set up axes
     ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.stock_img()
-    ax.add_feature(cartopy.feature.LAND)
-    ax.add_feature(cartopy.feature.OCEAN)
-    ax.add_feature(cartopy.feature.COASTLINE)
-    # plot points
-    for geodetic_coord in geodetic_coords:
-        plt.plot(geodetic_coord[0], geodetic_coord[1], color='blue', marker='o',
-                 transform=ccrs.PlateCarree())
     # zoom view around points
-    min_coord = np.min(geodetic_coords, axis=1) - 5
-    max_coord = np.max(geodetic_coords, axis=1) + 5
+    min_coord = np.min(geodetic_coords, axis=0) - 5
+    max_coord = np.max(geodetic_coords, axis=0) + 5
     ax.set_extent([min_coord[0], max_coord[0], min_coord[1], max_coord[1]],
                   crs=ccrs.PlateCarree())
+    # add imagery
+    ax.add_feature(cartopy.feature.LAND)
+    ax.add_feature(cartopy.feature.OCEAN)
+    # plot points
+    for geodetic_coord in geodetic_coords:
+        plt.plot(geodetic_coord[0], geodetic_coord[1], color='black', marker='o',
+                 transform=ccrs.PlateCarree())
     # display plot
     plt.show()
