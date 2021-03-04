dd0ab0b362aa118519fa19acd89ec97fd50710c1
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-03-04 13:20:38 -0500

    Vectorized Point Plotting

--- plot.py
+++ plot.py
@@ -16,8 +16,7 @@ def plot_geodetic(geodetic_coords):
     ax.add_feature(cartopy.feature.LAND)
     ax.add_feature(cartopy.feature.OCEAN)
     # plot points
-    for geodetic_coord in geodetic_coords:
-        plt.plot(geodetic_coord[0], geodetic_coord[1], color='black', marker='o',
-                 transform=ccrs.PlateCarree())
+    plt.scatter(geodetic_coords[:, 0], geodetic_coords[:, 1], color='black', marker='o',
+                transform=ccrs.PlateCarree())
     # display plot
     plt.show()
